5d244d0183d18a92bb240531f21b510241eaa622
the pandeybishal <47141238+BishalPandeycsb@users.noreply.github.com>
2023-03-21 14:58:01 +1100

    Add files via upload

---
+++ String.py
@@ -0,0 +1,5 @@
+s="the quick brown FOX jumps over the lazy dog"
+print(s.capitalize()) #first character of string is capitalise
+print(s.casefold())   #return all in small letter
+print(s.count('j'))   # counts the specific character and return its value
+print(s.find('brown')) #return the index of starting word else return -1
